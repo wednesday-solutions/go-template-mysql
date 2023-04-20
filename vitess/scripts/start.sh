8cbc0ec043d249ab149f39c091c2ac703119730f
Sourav Singh Rawat <sourav@wednesday.is>
2023-04-20 15:06:25 +0530

    fix: updated start script

--- vitess/scripts/start.sh
+++ vitess/scripts/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source ./utils.sh
+source ./vitess/scripts/utils.sh
 
 minikube start --kubernetes-version=v1.24.0 --cpus=4 --memory=8000 --disk-size=30g
 killall kubectl
-kubectl apply -f ../local/operator.yaml
+kubectl apply -f ./vitess/local/operator.yaml
 checkPodStatusWithTimeout "vitess-operator(.*)1/1(.*)Running(.*)"
 
-kubectl apply -f ../local/01-initial-cluster.yaml
+kubectl apply -f ./vitess/local/01-initial-cluster.yaml
 checkPodStatusWithTimeout "vitess-cluster-useast1-vtctld(.*)1/1(.*)Running(.*)"
 checkPodStatusWithTimeout "vitess-cluster-useast1-vtgate(.*)1/1(.*)Running(.*)"
 checkPodStatusWithTimeout "vitess-cluster-etcd(.*)1/1(.*)Running(.*)" 3
